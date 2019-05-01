c420284fc20d8ced8770ecad3fbc7e322b0e47f6
Kinga <fedorkinga99@gmail.com>
2019-05-01 19:43:22 +0200

    beadando

--- EX20.py
+++ EX20.py
@@ -1,1 +1,16 @@
 import sys
+
+def printSubStr(st, low, high):
+    sys.stdout.write(st[low: high + 1])
+    sys.stdout.flush()
+    return ''
+
+def longestPalSubstr(st):
+    n = len(st)
+    table = [[0 for x in range(n)] for y
+             in range(n)]
+    maxLength = 1
+    i = 0
+    while (i < n):
+        table[i][i] = True
+        i = i + 1
