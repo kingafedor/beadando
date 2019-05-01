d4804d02548b99e47bf6ebe04b50ffa5456438da
Kinga <fedorkinga99@gmail.com>
2019-05-01 19:46:29 +0200

    beadando

--- EX20.py
+++ EX20.py
@@ -9,8 +9,3 @@ def longestPalSubstr(st):
     n = len(st)
     table = [[0 for x in range(n)] for y
              in range(n)]
-    maxLength = 1
-    i = 0
-    while (i < n):
-        table[i][i] = True
-        i = i + 1
