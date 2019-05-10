43c89a9fb368adf29b0f194ec350e3d3f5c0a5fa
Kinga <fedorkinga99@gmail.com>
2019-05-10 19:46:36 +0200

    beadando

--- EX40.py
+++ EX40.py
@@ -9,3 +9,8 @@ def determ():
         c=np.randint(0,10)
         d=np.randint(0,10)
         if 10<= (a*d-b*c) <=20:
+            szamlalo += 1
+            print("a:", a, "b:", b, "c:", c, "d:", d)
+            tomb = [[a, b], [c, d]]
+            for i in tomb:
+                print(i)
