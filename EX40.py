daec48fdb0fb849045e8a232eb052a1ffafbba09
Kinga <fedorkinga99@gmail.com>
2019-05-10 17:10:37 +0200

    beadando

--- EX40.py
+++ EX40.py
@@ -3,3 +3,9 @@ import numpy.random as np
 def determ():
     tomb=[]
     szamlalo=0
+    while szamlalo!=5:
+        a=np.randint(0,10)
+        b=np.randint(0,10)
+        c=np.randint(0,10)
+        d=np.randint(0,10)
+        if 10<= (a*d-b*c) <=20:
